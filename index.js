a961033d24f21a3f1d1f248da82a88e8eed6a6e2
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-01 16:04:13 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+  
+function destructivelyAppendCat(name) {
+    cats.push(name);
+  }
+  console.log(cats);
+  
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+console.log(cats)
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  destructivelyRemoveLastCat(2)
+  console.log(cats);
+  
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  destructivelyRemoveFirstCat(0)
+  console.log(cats);
+
+  function appendCat(name) {
+    return [...cats, name]; // Returns a new array, does not modify `cats`
+  }
+  console.log(cats);
+
+  function prependCat(name) {
+    return [name, ...cats]; 
+  }
+  console.log(cats);
+
+  function removeLastCat() {
+    return cats.slice(0, -1)
+  }
+  console.log(cats);
+  
+  
+  function removeFirstCat() {
+    return cats.slice(1); 
+  }
+  console.log(cats);
